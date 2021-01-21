c99fe993bbfcaedd30d42eb01e0d6c729317258c
rizalrahardi <rizalrahardi@gmail.com>
2021-01-21 21:22:30 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function hitungLuas() {
+    let alas = prompt("Masukan Nilai Alas", );
+    console.log("Nilai Alas = ", alas);
+    let tinggi = prompt("Masukan Nilai Tinggi", );
+    console.log("Nilai Tinggi =", tinggi);
+    let hasil = prompt("Hasil Perhitungan 1/2x alas x tinggi",(alas*tinggi)*0.5 );
+    console.log("Luas Segitiga =", hasil);
+}
